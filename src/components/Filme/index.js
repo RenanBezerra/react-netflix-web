f4a054274abebea1ff2ed0cda698bc845976847c
Renan Bezerra Gomes <renanbg90@gmail.com>
2022-06-09 12:51:35 -0300

    1.3
    Create components, properties and use bootstrap
    https://www.bootstrapcdn.com/
    https://getbootstrap.com/docs/4.1/content/tables/

---
+++ src/components/Filme/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Filme = ({filme}) => {
+
+    const mostrarNome = () => {
+        alert(filme.nome)
+    }
+
+    return (
+        <tr>
+                <td>{filme.nome}</td>
+                <td>{filme.cateforia}</td>
+                <td>{filme.ano}</td>
+                <td className="text-right">
+                    <button className="btn btn-info" onClick={mostrarNome}>Informações</button>
+                    {' '}
+                    <button className="btn btn-danger">Excluir</button>
+                </td>
+        </tr>
+    )
+}
+
+export default Filme;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-
+import Filme from '../../components/Filme'
 const Home = () => {
+
+const listaFilmes = [
+    {nome: 'Filme 1', ano: 2020, cateforia: 'Terror'},
+    {nome: 'Filme 2', ano: 2021, cateforia: 'Terror'},
+    {nome: 'Filme 3', ano: 2022, cateforia: 'Terror'},
+    {nome: 'Filme 4', ano: 2023, cateforia: 'Terror'},
+    {nome: 'Filme 5', ano: 2024, cateforia: 'Terror'}
+]
+
     return (
-        <h1>RenanHome</h1>
+        <div className="container">
+            <br/>
+            <h1>Lista Filmes</h1>
+            <br/>
+            <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Categoria</th>
+                <th scope="col">Ano</th>
+                <th scope="col" className="text-right">Acoes</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            {listaFilmes.map(filme => (<Filme filme={filme}/>))}
+              
+            </tbody>
+            </table>
+        </div>
     );
 }
 
